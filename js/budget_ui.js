e08523222c06d8bb820ef0a8d8e03e05d87ab0ab
Mutahhir Hayat <mutahhir.ali@gmail.com>
2012-12-04 14:59:17 +0800

    Adding autocomplete, new income change method and more

--- js/budget_ui.js
+++ js/budget_ui.js
@@ -1,5 +1,6 @@
 $(function() {
 	planner = new BudgetPlanner(2000);
+	var itemArray = [];
 
 	$('#income').text(2000);
 
@@ -14,23 +15,57 @@ $(function() {
 		el.text([months[monthNum-1],yearNum].join(', '));
 	}
 
+	function cachePurseItems() {
+		var els = $('[data-provide="typeahead"]');
+		if (itemArray.length === 0) {
+			itemArray = ["Rent", "School Fees", "Groceries", "Utility", "Electricity", "Gas", "Water", "Savings", "Insurance", 
+			"Transport", "Entertainment", "Child care", "Day care", "Maintenance", "Rainy Day Fund",
+			"Dining out", "School Lunch", "Oil", "Cable", "Telephone", "Internet", "Satellite", "Garbage Pickup",
+			"Cell Phone", "House Cleaner", "Mortgage", "Tax", "Property Tax", "Property Insurance",
+			"Laundry Supplies", "Cleaning Supplies", "Furniture", "Appliances", "Linens", "Toiletries",
+			"Stationery", "Parking", "Medical", "Health Insurance", "Books", "Gifts", "Car", "Vacation", "Investment", 
+			"Movies", "Clothing", "Medicine"];
+		}
+
+		els.data('source', itemArray);
+
+	}
+
 	updateDateFromData($("#monthDescription"));
+	cachePurseItems();
 	
 	function updateStatement() {
 		$('#budgetStatement').html(function(index, oldHtml) {
 			var income = planner.totalIncome(),
 				planned = planner.totalPlanned();
-
+				incomeTemplate = 'Planning for a {{income}} income. <a id="changeIncome" href="#">Change?</a>',
+				nothingPlannedTemplate = "You&rsquo;ve not budgeted any of your income. Start below!",
+				somePlannedTemplate = 'You&rsquo;ve budgeted <span id="managedFigure" class="managed figure">{{planned}}</span>, but you still have <span id="unmanagedFigure" class="unmanaged figure">{{unplanned}}</span> left to budget',
+				overPlannedTemplate = 'Oops, looks like you&rsquo;re running short of money this month. You&rsquo;re <span class="unmanaged figure">{{over}}</span> over your income. Try adjusting your budget, or <a href="#">maybe take a loan from yourself.</a>';
+				fullyPlannedTemplate = 'Great! Your budget is fully balanced, now comes the hard part. <em>Stick to it!</em>',
+				wrapElement = '<h4>',
+				endWrapElement = '</h4>',
+				statement = '';
 
 			if (planned === 0) {
-				return Mustache.render('<h4>You&rsquo;ve not budgeted any of your income. Start below!</h4>', income);
+				statement = nothingPlannedTemplate;
 			} else if (planned < income) {
-				return Mustache.render('<h4>You&rsquo;ve budgeted <span id="managedFigure" class="managed figure">{{planned}}</span>, but you still have <span id="unmanagedFigure" class="unmanaged figure">{{unplanned}}</span> left to budget</h4>', {income: income, planned: planned, unplanned: income - planned});
+				statement = somePlannedTemplate;
 			} else if (planned > income){
-				return Mustache.render('<h4>Oops, looks like you&rsquo;re running short of money this month. You&rsquo;re <span class="unmanaged figure">{{over}}</span> over your income. Try adjusting your budget, or <a href="#">maybe take a loan from yourself</a></h4>.', {over: planned - income});
+				statement = overPlannedTemplate;
 			} else {
-				return "<h4>Great! Your budget is fully balanced, now comes the hard part. <em>Stick to it!</em></h4>";
+				statement = fullyPlannedTemplate;
 			}
+
+			var resultTemplate = "<p>" + incomeTemplate + "</p>" +
+				wrapElement + statement + endWrapElement;
+
+			return Mustache.render(resultTemplate, {
+				income: income,
+				planned: planned,
+				unplanned: income - planned,
+				over: planned - income
+			});
 		});
 	}
 
@@ -123,29 +158,54 @@ $(function() {
 		return false;
 	});
 
-	$('#changeIncome').click(function() {
-		var template = '<form id="editableIncomeArea" class="form form-inline" action="">' +
-				'<h3>Change income to '+
-					'<input id="incomeInput" type="text" class="income figure" value="{{.}}"></input>&nbsp;' +
-					'<button id="finishIncomeEditing" class="btn" type="submit">Change!</button>'+
-				'</h3>'+
-			'</form>';
+	$('#changeIncome').live('click', function() {
+		var modal = '<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">'+
+			'<div class="modal-header">' +
+				'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+				'<h3>Change Income</h3>' +
+			'</div>' +
+			'<div class="modal-body">' +
+				'<form id="editableIncomeArea" class="form form-inline" action="">' +
+					'<p>Change income to '+
+						'<input id="incomeInput" type="text" class="income figure" value="{{.}}"></input>&nbsp;' +
+					'</p>'+
+				'</form>' +
+			'</div>'+
+				'<div class="modal-footer">' +
+					'<a href="#" class="btn" data-dismiss="modal">Close</a>' +
+					'<a href="#" class="btn btn-primary" id="#saveNewIncome" data-dismiss="modal">Save changes</a>' +
+				'</div>' +
+			'</div>';
 
-		var $incomeArea = $('#incomeArea');
-		$incomeArea.css('display', 'none');
+		var $dest = $('#main');
 
-		var elem = $(Mustache.render(template, planner.totalIncome())).insertAfter($incomeArea);
+		var elem = $(Mustache.render(modal, planner.totalIncome())).appendTo($dest);
+		elem.modal('show');
 
-		$('#incomeInput').focus();
+		var save = false;
+		elem.on('shown', function() {
+			$('#incomeInput').focus();
+			$('#saveNewIncome').click(function() {
+				save = true;
+			});
+			$('#incomeInput').keydown(function(e) {
+				if (e.which === 13) {
+					save = true;
+					elem.modal('hide');
+				}
+			});
+		});
 
-		$('#finishIncomeEditing').click(function() {
-			var newIncome = $('#incomeInput').val();
-			planner.setIncome(newIncome);
-			$('#income').text(planner.totalIncome());
-			$incomeArea.css('display', '');
+		$(elem).on('hidden', function() {
+			if (save) {
+				var newIncome = $('#incomeInput').val();
+				planner.setIncome(newIncome);
+				updateStatement();
+				$('#headInput').focus();
+				updateStatement();
+			}
+
 			elem.remove();
-			$('#headInput').focus();
-			updateStatement();
 		});
 	});
 
